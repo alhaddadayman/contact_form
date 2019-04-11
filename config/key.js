383e1a2b4b59fb55b40baa427276e5252d2c678b
alhaddadayman <alhaddad.ay@gmail.com>
2019-04-11 10:49:19 +0200

    first

---
+++ config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    sendBackUser :"alhaddad.ayman91@gmail.com",
+    sendBackPass: "qlhbxfhuaprzwexm",
+    mialtrapHost: "smtp.mailtrap.io",
+    mailtrapUser: "f83594f08cb969",
+    mailtrapPass: "99d8142363be21"
+} 
